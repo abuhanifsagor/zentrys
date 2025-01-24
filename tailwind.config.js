e573e641f7fb9587cdcf8b6edfc818c53c8a4a81
abuhanifsagor <abuhanifsagor72@gmail.com>
2025-01-25 03:47:08 +0600

    try

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
     content: [".src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'blue': 'rgb(16, 24, 40)',
+        },
+        w: {
+          'bd': '140px',
+        },
+      },
     },
     plugins: [],
 }
